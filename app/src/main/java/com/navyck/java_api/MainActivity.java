066fa6b64cb396ef9541fe367e86d891ccd8bf24
navyCK <ckck81026@gmail.com>
2021-10-20 22:06:56 +0900

    Initial commit

---
+++ app/src/main/java/com/navyck/java_api/MainActivity.java
@@ -0,0 +1,17 @@
+package com.navyck.java_api;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Timer;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
