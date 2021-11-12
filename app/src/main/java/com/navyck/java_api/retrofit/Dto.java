3435ea4eb199b219e6275381e67e930a0d592299
navyCK <ckck81026@gmail.com>
2021-11-12 18:53:01 +0900

    [master] retrofit dto

---
+++ app/src/main/java/com/navyck/java_api/retrofit/Dto.java
@@ -0,0 +1,17 @@
+package com.navyck.java_api.retrofit;
+
+import java.util.List;
+
+public class Dto {
+
+}
+
+class Model {
+    int id;
+    String title;
+    String price;
+    Double lat;
+    Double lng;
+    String imgUrl;
+
+}
